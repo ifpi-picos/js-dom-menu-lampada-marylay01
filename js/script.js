e6aa77cfbb857ebfe575a793936773ed23795ffa
Reh <robinhoitaimcoelho@gmail.com>
2024-07-16 21:11:43 -0300

    ativ lampada

--- js/script.js
+++ js/script.js
@@ -1,4 +1,9 @@
 // Menu mobile
+function toggleMenu(){
+  const nav = document.querySelector("nav");   //selecionar 
+  nav.classList.toggle = ("mobile");             //perguntar
+
+}
 
 // Imagem 1
 const imagem1 = document.getElementById("lampada-01");
@@ -12,10 +17,10 @@ imagem1.addEventListener("click", function () {
 });
 
 // Imagem 2
-const imagem2 = document.getElementById("lampada-02");
-imagem2.addEventListener("mouseover", function () {
-   imagem2.src=("img/lampada.jpg")                        //vâo acontecer 2 eventos, ascender com o mouse em cima e apagar com  o mouse fora 
+const imagem2 = document.getElementById("lampada-02"); //pegar um elemento pelo ID dele no html   
+imagem2.addEventListener("mouseover", function () {         //adicionando os eventos addEventListener
+  imagem2.src = "img/lampada-on.jpg";                   //on para a imagem ascender    //vão acontecer 2 eventos, ascender com o mouse em cima e apagar com  o mouse fora 
 })
-imagem2.addEventListener("mouseout", function(){
-
+imagem2.addEventListener("mouseout", function() {
+   imagem2.src = "img/lampada.jpg";             //nao tem o "on" pois vai ficar desligada
 });
